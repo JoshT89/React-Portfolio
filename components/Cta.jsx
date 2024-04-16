89f7f83e07601e2e231c2447b3a5538afd97fadc
Joshua Turner <josh.h.t@live.co.uk>
2024-04-16 17:46:27 +0100

    added the footer with icons and copyright just need to link the pages up and attach my own info

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Cta = () => {
     return (
-        <section>
+        <section className='py-24 bg-tertiary dark:bg-secondary/40'>
             <div className='container mx-auto'>
                 <div className='flex flex-col items-center'>
                     <h2 className='h2 max-w-xl text-center mb-8'>Prepared to turn your ideas into reality? I'm here to help.</h2>
